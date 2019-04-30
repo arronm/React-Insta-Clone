539b20925a7a8eb22fbd06326c8e1d8efdffc047
Arron M <marshall.arron@gmail.com>
2019-04-30 15:36:42 -0700

    Refactor post to create like post function

--- instagram/src/components/PostContainer/Post.jsx
+++ instagram/src/components/PostContainer/Post.jsx
@@ -1,30 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import CommentSection from '../CommentSection/CommentSection';
 import './PostContainer.scss';
 
-const Post = (props) => {
-  return (
-    <article className="Post">
-      <header className="user">
-        <span className="user__thumbnail">
-          <img src={props.thumbnailUrl} alt={props.username}/>
-        </span>
-        <span className="user__name">{props.username}</span>
-      </header>
-      <div className="image">
-        <img src={props.imageUrl} alt={`by ${props.username}`} />
-      </div>
-      <div className="like-box">
-        <div className="icons">
-          <span className="icons__heart"></span>
-          <span className="icons__comment"></span>
+class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      thumbnailUrl: props.thumbnailUrl,
+      username: props.username,
+      imageUrl: props.imageUrl,
+      likes: props.likes,
+      timestamp: props.timestamp,
+      comments: props.comments,
+    }
+  }
+
+  handleLike = () => {
+    this.setState({
+      likes: this.state.likes + 1, // TODO: This will need to be refactored to handle removing your like
+    })
+  }
+
+  render() { 
+    return (
+      <article className="Post">
+        <header className="user">
+          <span className="user__thumbnail">
+            <img src={this.state.thumbnailUrl} alt={this.state.username}/>
+          </span>
+          <span className="user__name">{this.state.username}</span>
+        </header>
+        <div className="image">
+          <img src={this.state.imageUrl} alt={`by ${this.state.username}`} />
         </div>
-        <span className="likes">{`${props.likes} likes`}</span>
-      </div>
-      <CommentSection comments={props.comments} timestamp={props.timestamp} />
-    </article>
-  );
+        <div className="like-box">
+          <div className="icons">
+            <span className="icons__heart" onClick={this.handleLike}></span>
+            <span className="icons__comment"></span>
+          </div>
+          <span className="likes">{`${this.state.likes} likes`}</span>
+        </div>
+        <CommentSection comments={this.state.comments} timestamp={this.state.timestamp} />
+      </article>
+    );
+  }
 }
 
 Post.propTypes = {
