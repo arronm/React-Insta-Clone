9f674cdb87a895a4b81f36a8774ebc74f13aec73
Arron M <marshall.arron@gmail.com>
2019-05-01 08:28:41 -0700

    Refactor to controlled components with local state, save user to local storage

--- instagram/src/components/Login/Login.jsx
+++ instagram/src/components/Login/Login.jsx
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Login = (props) => {
-  const handleLogin = (event) => {
-    event.preventDefault();
-    console.log('You did it');
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    }
   }
-  return (
-    // TODO: Turn these into controlled inputs
-    <form onSubmit={handleLogin}>
-      <input type="text" name="username" placeholder="username" />
-      <input type="password" name="password" placeholder="password" />
-      <input type="submit" value="Login"/>
-    </form>
-  );
+
+  handleLogin = (event) => {
+    event.preventDefault();
+    window.localStorage.setItem('user', this.state.username)
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      // TODO: Turn these into controlled inputs
+      <form onSubmit={this.handleLogin}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={this.state.username}
+          onChange={this.handleOnChange}
+          />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={this.state.password}
+          onChange={this.handleOnChange}
+          />
+        <input type="submit" value="Login"/>
+      </form>
+    );
+  }
 }
  
 export default Login;
